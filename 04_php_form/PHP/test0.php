f4af2645b9d65e2d808fb9942efa15f2b79f42e1
TagayaUook <mancapitaltagaya@gmail.com>
2021-11-05 12:20:05 +0900

    if条件を$_POST["sitename"]に戻して検証確認

--- 04_php_form/PHP/test0.php
+++ 04_php_form/PHP/test0.php
@@ -2,12 +2,12 @@
 function Ch_name(){
 $log="";
 if(isset($_POST["sendin"])){
-$error=0; $ell=$_POST["sitename"];
-if(empty($ell)){
+$error=0;
+if(empty($_POST["sitename"])){
   $log= "この項目は入力必須です";
-}else if(mb_strlen($ell)>10){
+}else if(mb_strlen($_POST["sitename"])>10){
   $log= "10文字以内で入力してください";
-}else if(!in_array($ell,["TRYPHP"])){
+}else if(!in_array($_POST["sitename"],["TRYPHP"])){
   $log= "問題なし";
 }else{$error=1; $log=$error; /*結果：１ */}
 } print $log;}
