7fd03f1f7ece2ce22890a434053671357cad3b89
TagayaUook <mancapitaltagaya@gmail.com>
2021-11-05 15:31:05 +0900

    function　更新　print一括に

--- 04_php_form/PHP/contact.php
+++ 04_php_form/PHP/contact.php
@@ -5,31 +5,36 @@ function Ch_name(){   $name="";
   else if(mb_strlen($_POST["name"])>10){$name= "10文字以内で入力してください";}
   else if(!in_array($_POST,["山田太郎"])){}
   else{$name= "この項目は入力必須です";}
-}}
+  print $name;}
+}
 function Ch_kana(){   $kana="";
   if(isset($_POST["send_check"])){
-  if(empty($_POST["kana"])){print "この項目は入力必須です";}
-  else if(mb_strlen($_POST["kana"])>10){print "10文字以内で入力してください";}
+  if(empty($_POST["kana"])){$kana= "この項目は入力必須です";}
+  else if(mb_strlen($_POST["kana"])>10){$kana= "10文字以内で入力してください";}
   else if(!in_array($_POST,["ヤマダタロウ"])){}
-  else{$name= "この項目は入力必須です";}
-}}
+  else{$kana= "この項目は入力必須です";}
+  print $kana;}
+}
 function Ch_tel(){    $tel="";
   if(isset($_POST["send_check"])){
-  if(empty($_POST["tel"])){print "この項目は入力必須です";}
-  else if(mb_strlen($_POST["tel"])>10){print "10文字以内で入力してください";}
+  if(empty($_POST["tel"])){$tel= "この項目は入力必須です";}
+  else if(mb_strlen($_POST["tel"])>10){$tel= "10文字以内で入力してください";}
   else if(!in_array($_POST,["09012345678"])){}
-  else{$name= "この項目は入力必須です";}
-}}
+  else{$tel= "この項目は入力必須です";}
+  print $tel;}
+}
 function Ch_email(){   $email="";
   if(isset($_POST["send_check"])){
-  if(empty($_POST["Email"])){print "この項目は入力必須です";}
+  if(empty($_POST["Email"])){$email= "この項目は入力必須です";}
   else if(!in_array($_POST,["test@test.co.jp"])){}
-  else{$name= "この項目は入力必須です";}
-}}
+  else{$email= "この項目は入力必須です";}
+  print $email;}
+}
 function Ch_body(){   $body="";
   if(isset($_POST["send_check"])){
-  if(empty($_POST["body"])){print "この項目は入力必須です";}
-}}
+  if(empty($_POST["body"])){$body= "この項目は入力必須です";}
+  print $body;}
+}
 
 ?>
 <!DOCTYPE html>
@@ -57,7 +62,7 @@ function Ch_body(){   $body="";
   <p><span class="required">*</span>は必須項目となります。</p>
   <dl>
     <dt><label for="name">氏名</label>
-      <span class="required">*<?php echo Ch_name(); ?></span></dt>
+      <span class="required">*</span></dt>
       <dd><input type="text" name="name" id="In_name" placeholder="山田太郎"></dd>
     <dd><label for="kana">フリガナ</label>
       <span class="required">*</span></dt>
