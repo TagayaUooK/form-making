29757a10f99c5263fee424e7654a6b47562a15be
TagayaUook <mancapitaltagaya@gmail.com>
2021-11-10 15:50:08 +0900

    $count削除（理由：エラー発生につき）

--- 04_php_form/PHP/contact.php
+++ 04_php_form/PHP/contact.php
@@ -1,10 +1,9 @@
 <?php
-$count=0;
 function Ch_name(){   $name="";
   if(isset($_POST["send_check"])){
   if(empty($_POST["name"])){$name= "この項目は入力必須です";}
   else if(mb_strlen($_POST["name"])>10){$name= "10文字以内で入力してください";}
-  else if(!in_array($_POST,["山田太郎"])){$count+=1;}
+  else if(!in_array($_POST,["山田太郎"])){}
   else{$name= "この項目は入力必須です";}
   print $name;}
 }
@@ -12,7 +11,7 @@ function Ch_kana(){   $kana="";
   if(isset($_POST["send_check"])){
   if(empty($_POST["kana"])){$kana= "この項目は入力必須です";}
   else if(mb_strlen($_POST["kana"])>10){$kana= "10文字以内で入力してください";}
-  else if(!in_array($_POST,["ヤマダタロウ"])){$count+=1;}
+  else if(!in_array($_POST,["ヤマダタロウ"])){}
   else{$kana= "この項目は入力必須です";}
   print $kana;}
 }
@@ -20,21 +19,21 @@ function Ch_tel(){    $tel="";
   if(isset($_POST["send_check"])){
   if(empty($_POST["tel"])){$tel= "この項目は入力必須です";}
   else if(mb_strlen($_POST["tel"])>10){$tel= "10文字以内で入力してください";}
-  else if(!in_array($_POST,["09012345678"])){$count+=1;}
+  else if(!in_array($_POST,["09012345678"])){}
   else{$tel= "この項目は入力必須です";}
   print $tel;}
 }
 function Ch_email(){   $email="";
   if(isset($_POST["send_check"])){
   if(empty($_POST["Email"])){$email= "この項目は入力必須です";}
-  else if(!in_array($_POST,["test@test.co.jp"])){$count+=1;}
+  else if(!in_array($_POST,["test@test.co.jp"])){}
   else{$email= "この項目は入力必須です";}
   print $email;}
 }
 function Ch_body(){   $body="";
   if(isset($_POST["send_check"])){
   if(empty($_POST["body"])){$body= "この項目は入力必須です";}
-  else{$count+=1
+  else{}
   print $body;}
 }
 /*
@@ -92,6 +91,7 @@ function Ch_body(){   $body="";
 	  <dd><textarea name="body" id="In_body"></textarea></dd>
 	  <dd>
       <button type="submit" name="send_check" id="send_check">送信</button>
+      
     </dd>
   </dl>
 </form></div>
